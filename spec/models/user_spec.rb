35dd909f064554c01969c9662880ce5f01ecf815
Mojgan <modj.gan@gmail.com>
2013-10-10 22:02:22 -0400

    finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -95,7 +96,7 @@ describe User do
   describe "return value of authenticate method" do
   before { @user.save }
   let(:found_user) { User.find_by(email: @user.email) }
-
+  
   describe "with valid password" do
     it { should eq found_user.authenticate(@user.password) }
   end
@@ -105,6 +106,13 @@ describe User do
 
     it { should_not eq user_for_invalid_password }
     specify { expect(user_for_invalid_password).to be_false }
+    end
   end
- end
+ 
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
+
+
